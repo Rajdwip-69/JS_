920cd18ca31ba402b4475c5158b33c282ff64b89
Rajdwip-69 <rajdwipghosh27@gmail.com>
2024-08-15 12:48:43 +0530

    Basics of JavaScript

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+let a = 10 
+let b = 3
+let c = a+b
+console.log(c);
+console.log(a-b);
+console.log(a*b)
+console.log(a**b)
+console.log(a == b);
+
+// alert(3+5)
+
+window.alert("Hello Rajdwip")
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("Rajdwip")
+let accountId = 12345;
+let accountName = "Rajdwip"
+let accountCity = "Kolkata"
+
+console.table([accountId,accountName,accountCity])
