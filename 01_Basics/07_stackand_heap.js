d144e409e63b7820a83a9944be06333d7b8fb89c
Rajdwip-69 <rajdwipghosh27@gmail.com>
2024-08-18 23:02:45 +0530

    Second Commit

---
+++ 01_Basics/07_stackand_heap.js
@@ -0,0 +1,24 @@
+// Stack play Premitive Data Types
+// Heap play with Non primitive Data Types
+
+let myYoutubename = "rajdwipghosh"
+let annotherName = myYoutubename
+annotherName = "ranaghosh"
+// console.log(myYoutubename);
+// console.log(annotherName);
+
+
+// Heap Concept
+
+let userOne = {
+    email : "rajdwipghosh27@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "rajdwip8000@gamil.com"
+
+console.log(userOne)
+console.log(userTwo);
+
+
---
+++ 01_Basics/08_string.js
@@ -0,0 +1,47 @@
+const name = "Rajdwip"
+const repoCount = 60
+// console.log(`Hello my name is ${name} my repocunt is ${repoCount}`);
+
+
+// Using object 
+
+const gameName = new String('  Rajdwipvc')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+//Length Measure 
+
+// console.log(gameName.length);
+
+// Uppdercase and lowecase conversion
+// console.log(gameName.toUpperCase());
+
+//Includes search a particlar word in the string and return Boolean
+
+const name1 = "the fox is claver "
+// console.log(name1.includes("fox"));
+
+// charAt returns the posation of charecter input is intiger
+// console.log(gameName.charAt(2));
+//endsWith check the ending of the string and return Boolean
+// console.log(gameName.endsWith('c'));
+// Trimstart is delete the white space in the starting of the string
+// console.log(gameName.trimStart());
+
+
+// Trim also delete the whitespace from the string in any posation
+const name2 = "  desi  "
+// console.log(name2.trim());
+
+
+console.log(typeof name2.valueOf());
+
+
+
+
+
+
+
+
+
+
