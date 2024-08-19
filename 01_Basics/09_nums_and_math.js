8250d3850b046f740400790e7a6c5602e11eaf5d
Rajdwip-69 <143189655+Rajdwip-69@users.noreply.github.com>
2024-08-19 07:59:30 +0000

    Add files via upload

---
+++ 01_Basics/09_nums_and_math.js
@@ -0,0 +1,38 @@
+// +++++++++++++++++++++Nums+++++++++++++++++++++++++++++++++
+
+const score = 400
+// console.log(score);
+
+
+let value = new Number(400)
+
+// console.log(value);
+
+// console.log( typeof value.toString());
+
+// console.log(value.toPrecision(3));
+
+// Conver the number into string
+
+// console.log( typeof value.toLocaleString());
+
+const hundred = 10000000
+// conver like indian currency
+// console.log(hundred.toLocaleString('en-IN'));
+
+// ++++++++++++++++Math++++++++++++++++++++++++++++++++
+
+// console.log(Math.floor(4.6));
+// console.log(Math.abs(-7));
+// console.log(Math.PI);
+// console.log(Math.random());
+console.log(Math.max(2,7,9,10));
+console.log(Math.min(2,7,9,10));
+
+
+
+
+
+
+
+
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 console.log("Rajdwip")
+let accountId = 12345;
+let accountName = "Rajdwip"
+let accountCity = "Kolkata"
+
+console.table([accountId,accountName,accountCity])
+=======
+console.log("Rajdwip")
+>>>>>>> 33e1969 (Third Commit)
